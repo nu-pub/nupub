3fc7da2820d83b4f8d625099da81ba11a1b08d3a
Aomi Jokoji <ajokoji@change.org>
2021-01-09 18:55:07 -0800

    add temp styling playground

--- src/views/Routes.js
+++ src/views/Routes.js
@@ -1,27 +1,30 @@
 // the main routes of our app are defined here using react-router
 // https://reacttraining.com/react-router/web/example/basic
 
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import BlockPage from '../views/block/blockPage'
-import ChannelPage from '../views/channel/channelPage'
-import ProfilePage from '../views/profile/profilePage'
-import SourcePage from '../views/source/sourcePage'
-import SourceSearchPage from '../views/source/sourceSearchPage'
-import Error from '../views/misc/Error'
-
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import BlockPage from "../views/block/blockPage";
+import ChannelPage from "../views/channel/channelPage";
+import ProfilePage from "../views/profile/profilePage";
+import SourcePage from "../views/source/sourcePage";
+import SourceSearchPage from "../views/source/sourceSearchPage";
+// TODO: remove
+import StylePage from "./components/StylePage";
+import Error from "../views/misc/Error";
 
 //get user name if signed in
 
 const Routes = (props) => (
   <Switch>
-    <Route exact path="/" component={SourceSearchPage}/>
+    <Route exact path="/" component={SourceSearchPage} />
     <Route path="/user/:userId" component={ProfilePage} />
-    <Route path="/channel/:channelId" component={ChannelPage}/>
+    <Route path="/channel/:channelId" component={ChannelPage} />
     <Route path="/block/:blockId" component={BlockPage} />
     <Route path="/source/:sourceId" component={SourcePage} />
+    {/* TODO: remove before deploy */}
+    <Route path="/_/style" component={StylePage} />
     <Route component={Error} />
   </Switch>
-)
+);
 
-export default Routes
+export default Routes;
---
+++ src/views/components/StylePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Paragraph from "./Paragraph";
+
+const StylePage = () => (
+  <Paragraph>
+    Voluptate duis id quis amet. Qui consequat laborum eiusmod fugiat est et
+    enim Lorem. Irure ea velit eu eu labore. Nostrud sunt eiusmod deserunt
+    deserunt cillum.
+  </Paragraph>
+);
+
+export default StylePage;
